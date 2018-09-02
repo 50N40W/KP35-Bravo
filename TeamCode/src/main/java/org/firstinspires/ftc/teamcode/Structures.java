de0a4cd003c87ef0bc32e8f864a5a03de7211d27
Howard <bishop038@hotmail.com>
2018-09-01 21:21:45 -0400

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Structures.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+/* A set of data that should be common across all autonomous op modes
+ * Created by Howard on 12/28/2017.
+ */
+
+class Structures {
+    static final int CLM = 0;
+    static final int MOV = 2;
+    static final int LFT = 3;
+    static final int WT = 4;
+    final int CL = 1;
+    final int UC = -1;
+    final int UP = 1;
+
+    int[] mode;
+    int[] clamp;
+    int[] vLift;
+    int[] heading;
+    float[] vFwd;
+    float[] vCrab;
+    float[] vPwr;
+    long[] dur;
+    long currTime;
+    float leftX;
+    float leftY;
+    float rightX;
+    int status;
+}
